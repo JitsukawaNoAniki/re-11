310f1cdfdb28caa04fd7d2588837315fd16387d0
JitsukawaNoAniki <88788547+JitsukawaNoAniki@users.noreply.github.com>
2022-05-26 20:00:39 -0300

    tets

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ http.createServer(app).listen(app.get('port'), function(){
     console.log('Express Server escutando na porta ' + app.get('port'));
 });
 
-const url = 'mongodb://dswa5:dswa5@clusterdswa5-shard-00-00.oqbir.mongodb.net:27017,cluster0-shard-00-01.oqbir.mongodb.net:27017,cluster0-shard-00-02.oqbir.mongodb.net:27017/ifsp?ssl=true&replicaSet=atlas-rg0np8-shard-0&authSource=admin&retryWrites=true&w=majority';
+const url = 'mongodb+srv://dswa5:dswa5@clusterdswa5.x96ld.mongodb.net/ifsp?retryWrites=true&w=majority';
 require('./config/database.js')(url);
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express Server escutando na porta ' + app.get('port'));
